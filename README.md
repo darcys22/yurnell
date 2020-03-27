7103de60429aa9244c6ceb5c65b83c9f71801965
Sean Darcy <darcys22@gmail.com>
2020-03-27 22:10:39 +0000

    more passes over truffle objects converting to yurnell

--- README.md
+++ README.md
@@ -20,3 +20,5 @@ yurnell migrate
 
 ### Truffle Early Version for template
 https://github.com/trufflesuite/truffle/tree/1f00dcce4348b2970aab895130d29b8d92d25acc/packages/truffle-core
+
+
--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var pkg = require("./package.json");
 module.exports = {
   create: require("./lib/create"),
   config: require("./lib/config"),
-  journals: require("./lib/journals"),
   migrate: require("./lib/migrate"),
   version: pkg.version
 };

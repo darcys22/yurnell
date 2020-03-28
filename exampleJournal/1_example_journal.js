292779be4c73ce1a5b255937235e5a2d84ed0db2
Sean Darcy <darcys22@gmail.com>
2020-03-28 04:00:08 +0000

    have a variable that is accessable in the migration

--- exampleJournal/1_example_journal.js
+++ exampleJournal/1_example_journal.js
@@ -1,4 +1,8 @@
+//var MyContract = artifacts.require("MyContract");
+var MyJournal = journal;
+
 module.exports = function(deployer) {
+  console.log(MyJournal);
   // deployment steps
   deployer.deploy();
 };

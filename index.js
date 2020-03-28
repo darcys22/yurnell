cbf71165310f82f3bdd07def9498fb100d0ad8e0
Sean Darcy <darcys22@gmail.com>
2020-03-28 06:26:50 +0000

    Culled back a lot of the junk I had copied into the codebase

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var pkg = require("./package.json");
 
 module.exports = {
-  create: require("./lib/create"),
   config: require("./lib/config"),
   migrate: require("./lib/migrate"),
   version: pkg.version

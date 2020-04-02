c9a555da80f29e8bdfac91ea6c2c5854189c254b
Sean Darcy <darcys22@gmail.com>
2020-04-02 23:20:18 +0000

    Adding some key journal functions

--- exampleJournal/1_example_journal.js
+++ exampleJournal/1_example_journal.js
@@ -1,10 +1,16 @@
-//var MyContract = artifacts.require("MyContract");
 var MyJournal = Yurnell.newJournal();
 
 module.exports = function(deployer) {
-  MyJournal.test()
   MyJournal.description = "my first description"
-  console.log(MyJournal.description)
+
+  lineitem = Yurnell.newLineItem();
+  lineitem.particulars = "something particular";
+  lineitem.amount = 10;
+  lineitem.account = "Cash"
+
+  MyJournal.add(lineitem)
+  MyJournal.add(lineitem)
+  console.log(typeof MyJournal.date)
   // deployment steps
   deployer.deploy();
 };

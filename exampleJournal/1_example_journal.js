71e456f6aa7da264dc5520edb6542b7f31c8a4c9
Sean Darcy <darcys22@gmail.com>
2020-04-02 10:48:22 +0000

    Actually can play with the journal in the migration

--- exampleJournal/1_example_journal.js
+++ exampleJournal/1_example_journal.js
@@ -3,6 +3,8 @@ var MyJournal = Yurnell.newJournal();
 
 module.exports = function(deployer) {
   MyJournal.test()
+  MyJournal.description = "my first description"
+  console.log(MyJournal.description)
   // deployment steps
   deployer.deploy();
 };

db6ca1f8d3b56715b4bc1b5f0f55e166d479ff65
Sean Darcy <darcys22@gmail.com>
2020-04-03 02:32:42 +0000

    journal is looking much better

--- exampleJournal/1_example_journal.js
+++ exampleJournal/1_example_journal.js
@@ -3,14 +3,23 @@ var MyJournal = Yurnell.newJournal();
 module.exports = function(deployer) {
   MyJournal.description = "my first description"
 
+  MyJournal.date = "30 june 2019"
+
   lineitem = Yurnell.newLineItem();
   lineitem.particulars = "something particular";
-  lineitem.amount = 10;
-  lineitem.account = "Cash"
+  lineitem.amount = -10;
+  lineitem.account = "Income";
+  MyJournal.add(lineitem);
 
-  MyJournal.add(lineitem)
-  MyJournal.add(lineitem)
-  console.log(typeof MyJournal.date)
+  MyJournal.add(Yurnell.newLineItem({
+    particulars: "something particular",
+    amount: 10,
+    account: "Cash"
+  }));
+
+  console.log(MyJournal.date.toString())
+  console.log(MyJournal.length)
+  console.log(MyJournal.valid)
   // deployment steps
   deployer.deploy();
 };
